5bd1f08d6f715a64b1f0431a50947051c6d7b779
Mateusz Karoń <mateuszkaron96@gmail.com>
2025-03-19 23:03:50 +0100

    .

--- src/render.js
+++ src/render.js
@@ -1,2 +1,72 @@
 const city = document.getElementById("city");
 const _apiKey = "fVrDhhLOZdWRehiImpRkgrcEHAwYFU3r";
+
+
+// Get location key
+async function getLocationKey(city) 
+{
+    const cityUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${_apiKey}&q=${city}";
+ 
+    try{
+        const response = await fetch(cityUrl);
+
+        if(!response.ok){
+            throw new Error("We failed to get your city infomation");
+        }
+
+        const data = await response.json();
+        
+        if(data.length > 0){
+            const locationKey = data[0].Key;
+            return locationKey;
+        }else{
+            throw new Error("Your city is out of range");
+        }
+
+    }catch(error){
+        console.error("Error: ", error);
+        return error;
+    }
+}
+
+// Get weather
+async function getWeather(locationKey) 
+{
+    const weatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${_apiKey}";
+
+    try{
+        const response = await fetch(weatherUrl);
+
+        if(!response.ok){
+            throw new Error("We failed to get your weather infomation");
+        }
+
+        const data = await response.json();
+        const temperatureCelcius = data[0].Temperature.Metric.Value;  // Get temperature in Celcius
+        const temperatureFarenheit = data[0].Temperature.Imperial.Value; // Get temperature in Farenheit
+
+        return {
+            temperatureCelcius,
+            temperatureFarenheit 
+        };
+
+    } catch(error){
+        console.error("Error: ", error);
+        return error;
+    }
+}
+
+async function showWeather(city)
+{
+    try{
+        const locationKey = await getLocationKey(city);
+        const {temperatureCelcius, temperatureFarenheit} = await getWeather(locationKey);
+        
+        console.log("Temperature in Celcius: ", temperatureCelcius);
+        console.log("Temperature in Farenheit: ", temperatureFarenheit);
+
+    }catch(error){
+        console.error("Error: ", error);
+        return error;
+    }
+}
